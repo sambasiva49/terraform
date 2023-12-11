1c9a971f743508c13e1fa774a99cc2eae83b4cb9
samba <gudurusamba@gmail.com>
2023-12-11 10:20:15 +0530

    first expeirment

--- main1.tf
+++ main1.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  credentials = file("C:/Users/samba/Desktop/json/rich-principle-403808-2e2c18753f24.json")
+  credentials = "default"
   project     = "rich-principle-403808"
   region      = "us-central1-a"
 }
-
 
 resource "google_storage_bucket" "my_bucket" {
   name     = "my-unique-bucket-name"
   location = "us-central1-a"
 }
+
