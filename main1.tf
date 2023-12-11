3a3089600ba7b249b5696e9d54049f2327107b89
samba <gudurusamba@gmail.com>
2023-12-11 10:39:23 +0530

    first expeirment

--- main1.tf
+++ main1.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  credentials = "${file("credential.json")}"
+  credentials = file("credential.json")
   project     = "rich-principle-403808"
   region      = "us-central1-a"
 }
